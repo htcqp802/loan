a246ae3fc407bded4c2ba62f141b9511cb32b09f
116573916@qq.com <116573916@qq.com>
2016-06-24 17:40:54 +0800

    Merge branch 'dev' of git.fengjr.inc:feng-web/feng-loan into dev
    # Conflicts:
    #	src/components/index.js
    #	src/containers/Home/Home.js

--- src/containers/Home/Home.js
+++ src/containers/Home/Home.js
@@ -1,6 +1,6 @@
 import React,{Component} from 'react';
+import {Slider} from 'components';
 export default class Home extends Component{
-
     render(){
         const style = require("./Home.scss");
         return (
